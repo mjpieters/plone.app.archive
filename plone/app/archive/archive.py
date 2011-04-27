044258276211a949780b0536a58309ecb48d121b
Martijn Pieters <mj@zopatista.com>
2011-04-27 16:55:51 +0200

    Utility is a delegating wrapper to an annotation on the current site.

--- plone/app/archive/archive.py
+++ plone/app/archive/archive.py
@@ -28,9 +28,55 @@
 #   - Not making this a classic ZMI-based tool let's us avoid all the classic
 #     Zope2 sit-ups and the possibility for acquisition context and traversal
 #     to trigger all the wrong plone site hooks.
-from zope import interface
+from persistent import Persistent
+from zope import annotation, component, interface
+from Products.CMFCore.interfaces import ISiteRoot
 
 from plone.app.archive.interfaces import IContentArchive
 
+
+ARCHIVE_KEY = 'plone.app.archive'
 class ContentArchive(object):
+    """Wrapper for ArchivedContent
+
+    Looks up the local (per-site) ArchivedContent instance and delegates all
+    API calls to it.
+
+    """
     interface.implements(IContentArchive)
+
+    @property
+    def _archive(self):
+        site = component.getUtility(ISiteRoot)
+        ann = annotation.interfaces.IAnnotations(site)
+        if ARCHIVE_KEY not in ann:
+            ann[ARCHIVE_KEY] = ArchivedContent()
+        return ann[ARCHIVE_KEY]
+
+    def archiveContent(self, content):
+        return self._archive.archiveContent(content)
+
+    def restoreContent(self, id):
+        return self._archive.restoreContent(id)
+
+    def listArchivedContent(self, **filters):
+        return self._archive.listArchivedContent(**filters)
+
+    def clearArchive(self, **filters):
+        return self._archive.clearArchive(**filters)
+
+
+class ArchivedContent(Persistent):
+    interface.implements(IContentArchive)
+
+    def archiveContent(self, content):
+        pass
+
+    def restoreContent(self, id):
+        pass
+
+    def listArchivedContent(self, **filters):
+        pass
+
+    def clearArchive(self, **filters):
+        pass
